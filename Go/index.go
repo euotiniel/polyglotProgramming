9b11b99e74332dcc363319fef55fbd96ac3e63a0
Otoniel Emanuel <otonielemanuel21@gmail.com>
2023-04-04 11:50:12 +0100

    first commit 04/04/2023 - 11:48

--- Go/index.go
+++ Go/index.go
@@ -6,6 +6,4 @@ func main() {
     fmt.Println("Hello, World!")
     fmt.Println("Hello, World!")
     fmt.Println("Hello, World!")
-    fmt.Println("Hello, World!")
-    fmt.Println("Hello, World!")
 }
